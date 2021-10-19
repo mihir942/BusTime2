e93253e68b8651ec66e2796f9e875d625a113e0d
Mihir <mihirniyogi942@gmail.com>
2021-10-19 14:37:07 +0800

    First commit

---
+++ app/src/main/java/com/pmapps/bustime2/MainActivity.java
@@ -0,0 +1,15 @@
+package com.pmapps.bustime2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    
+}
