b4536e715ab7092c47a7e03580d79dcd44bb6bfc
Mihir <mihirniyogi942@gmail.com>
2021-10-20 21:13:21 +0800

    Fragments created, BottomNavBar is now fully functional.

--- app/src/main/java/com/pmapps/bustime2/MainActivity.java
+++ app/src/main/java/com/pmapps/bustime2/MainActivity.java
@@ -1,15 +1,37 @@
 package com.pmapps.bustime2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
+import com.pmapps.bustime2.NavigationFragments.FavouritesFragment;
+import com.pmapps.bustime2.NavigationFragments.NearbyFragment;
+import com.pmapps.bustime2.NavigationFragments.SettingsFragment;
+
 public class MainActivity extends AppCompatActivity {
+
+    ChipNavigationBar bottomNavBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavBar = findViewById(R.id.bottomNavBar);
+        bottomNavBar.setItemSelected(R.id.item0,true);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FavouritesFragment()).commit();
+        bottomNavBar.setOnItemSelectedListener(i -> {
+            Fragment fragment = null;
+            if (i == R.id.item0) fragment = new FavouritesFragment();
+            else if (i == R.id.item1) fragment = new NearbyFragment();
+            else if (i == R.id.item2) fragment = new SettingsFragment();
+            if (fragment != null) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,fragment).commit();
+            }
+        });
+
     }
     
 }
---
+++ app/src/main/java/com/pmapps/bustime2/NavigationFragments/SettingsFragment.java
@@ -0,0 +1,25 @@
+package com.pmapps.bustime2.NavigationFragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.pmapps.bustime2.R;
+
+public class SettingsFragment extends Fragment {
+
+    public SettingsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+}
